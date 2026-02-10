6f05ae9084f12127cf827e3e80ce3a6413166513
xieby1 <xieby1@outlook.com>
2026-02-10 10:56:21 +0800

    nestemp: clean

--- default.nix
+++ default.nix
@@ -20,7 +20,27 @@ in buildLocalTypstEnv (finalAttrs: {
     pkgs.typst
     pkgs.typstPackages.fletcher
   ];
-  propagatedBuildInputs = [
-    (import ./nestemp { inherit pkgs buildLocalTypstEnv; })
+  propagatedBuildInputs =
+  # propagatedBuildInputs for nestoc
+  [ ]
+  # propagatedBuildInputs for nestemp
+  ++ [
+    # Typst packages
+    (pkgs.typstPackages.ilm.overrideAttrs (finalAttrs: prevAttrs: {
+      version = "1.4.2";
+      src = pkgs.fetchzip {
+        hash = "sha256-aELsI13NxkUbjqBR363Wwzd0eJ8UzP1mLsQ28+z8qbg=";
+        url = "https://packages.typst.org/preview/${finalAttrs.pname}-${finalAttrs.version}.tar.gz";
+        stripRoot = false;
+      };
+      prePatch = ''
+        sed -i '/compiler/d' typst.toml
+      '';
+    }))
+    pkgs.typstPackages.glossy
+
+    # Fonts
+    (pkgs.callPackage ./nestemp/noto-fonts-cjk-sc-static.nix {})
+    pkgs.noto-fonts-color-emoji
   ];
 })
