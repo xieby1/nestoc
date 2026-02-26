8a986ad5e0eea3c80533e2df5c0adb50f296fa65
xieby1 <xieby1@outlook.com>
2026-02-26 13:25:41 +0800

    clean unused fonts

--- default.nix
+++ default.nix
@@ -29,7 +29,22 @@ in buildLocalTypstEnv (finalAttrs: {
     pkgs.typstPackages.glossy
 
     # Fonts
-    (pkgs.callPackage ./nestemp/noto-fonts-cjk-sc-static.nix {})
+    (let
+      base-url = "https://github.com/notofonts/noto-cjk/raw/f8d157532fbfaeda587e826d4cd5b21a49186f7c";
+    in pkgs.runCommand "noto" {} ([''
+      mkdir -p $out/share/fonts/noto
+    ''] ++ (map (font: ''
+      ln -s ${font} $out/share/fonts/noto/${font.name}
+    '') [
+      (pkgs.fetchurl {
+        url = "${base-url}/Serif/OTF/SimplifiedChinese/NotoSerifCJKsc-Regular.otf";
+        sha256 = "1jhjin8yngzjngl6l9f5cawc2cm51zi43301ain5b0yz50kawbia";
+      })
+      (pkgs.fetchurl {
+        url = "${base-url}/Serif/OTF/SimplifiedChinese/NotoSerifCJKsc-Bold.otf";
+        sha256 = "0wzw70xrq3pym7md5apl4ja1pwcmybm6dq1h5b3vr0ifdi5pvw4a";
+      })
+    ])))
     pkgs.noto-fonts-color-emoji
     (pkgs.fetchzip {
       url = "https://github.com/SpaceTimee/Fusion-JetBrainsMapleMono/releases/download/1.2304.79/JetBrainsMapleMono-NF-XX-NL-XX.zip";
